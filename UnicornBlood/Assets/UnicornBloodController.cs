ab3ff4aa23160099e2d2f5c55b4a04ca5b40f8b7
Johannes Päivinen <johannes.paivinen@nextgames.com>
2016-01-30 17:31:40 +0200

    Mar stuff

--- UnicornBlood/Assets/UnicornBloodController.cs
+++ UnicornBlood/Assets/UnicornBloodController.cs
@@ -6,13 +6,12 @@ public class UnicornBloodController : MonoBehaviour {
 	SpriteRenderer spriteRenderer = null;
 	// Use this for initialization
 	void Start () {
-		phase = 0;//transform.position.x + transform.position.y;
+		phase = 4;//transform.position.x + transform.position.y;
 		spriteRenderer = GetComponent<SpriteRenderer> ();
 	}
 
 	Color GetRainbow(float f)
 	{
-
 		if (f < 0.25f) 
 			return Color.Lerp (Color.red, Color.yellow, (f-0)*4);
 		
@@ -30,10 +29,6 @@ public class UnicornBloodController : MonoBehaviour {
 	{
 		var pos = transform.position;
 		phase += Time.deltaTime;
-		/*
-		float r = phase - (pos.x + pos.y)*5.777f;
-		float g = phase*0.7f + pos.x*5.111f+0.177f;
-		float b = phase*1.11f + pos.y*2 * 1.333f;*/
 		float ph = phase + pos.x - pos.y;
 		float m = ph % 1.0f;
 		spriteRenderer.color = GetRainbow (m);
